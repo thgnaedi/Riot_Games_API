367c5d1b7f335c640bbba7c9fde822ba306361cd
thgnaedi <cntu4@web.de>
2018-02-21 14:48:56 +0100

    =)

--- src/controller/RitoAPI.java
+++ src/controller/RitoAPI.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import model.History;
 import model.connect_to_rito;
 import model.Summoner;
 
@@ -21,14 +17,16 @@ public class RitoAPI {
         try {
             connect_to_rito rt = new connect_to_rito(DO_NOT_PUBLISH.KEY);
             Summoner sum = rt.get_summoner("PoroKönig Blade");
-            if(sum != null){
-            System.out.println(sum);
-            //History hist = rt.get_gameHistory(sum);
+            if (sum != null) {
+                System.out.println(sum);
+                History hist = rt.get_gameHistory(sum);
+                rt.get_all_Champions();
+                hist.get_Most_Played_Champions();
             }
-            rt.get_Champions();
+
         } catch (Exception ex) {
             System.out.println("controller.RitoAPI.main()" + "\n\t" + ex);
-            System.out.println("last HTTP error "+connect_to_rito.last_error);
+            System.out.println("last HTTP error " + connect_to_rito.last_error);
         }
 
     }
